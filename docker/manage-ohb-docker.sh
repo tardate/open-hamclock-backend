b95c3097f795d3cb3b65678fe4e8da013b34c889
Dave Koberstein <davek@komacke.com>
2026-03-12 18:10:31 -0700

    Fix voacap tag

--- docker/manage-ohb-docker.sh
+++ docker/manage-ohb-docker.sh
@@ -868,7 +868,7 @@ services:
         condition: service_healthy
 
   voacap-service:
-    image: komacke/voacap-service:l.3
+    image: komacke/voacap-service:1.3
     container_name: voacap-service
     restart: unless-stopped
     environment:
