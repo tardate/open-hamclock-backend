cd0512b9d7083514deac9d882c97480278db22e4
Dave Koberstein <davek@komacke.com>
2026-03-26 23:01:50 -0700

    Start using dynamic version.pl

--- ham/HamClock/version.pl
+++ ham/HamClock/version.pl
@@ -1,8 +1,34 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl
 use strict;
 use warnings;
+use CGI;
 
-print "Remote_Addr: $ENV{REMOTE_ADDR}\n";
-print "Content-Type: text/plain; charset=ISO-8859-1\r\n\r\n";
-print "4.22\n";
-print "No info for OHB version  4.22\n";
+my $q = CGI->new;
+my $cache_dir = "/opt/hamclock-backend/cache";
+
+print $q->header('text/plain');
+
+# Parse User-Agent (e.g., HamClock/4.22.1b01)
+my $ua_string = $q->user_agent() || "";
+
+# We just need to check if there is a version string at all
+my ($client_ver) = $ua_string =~ m|/(\d+\.[\w\.]+)|;
+
+unless ($client_ver) {
+    print "Unknown\n";
+    exit;
+}
+
+# Determine file based on 'b' in the raw client version string
+my $type = ($client_ver =~ /b/i) ? "beta" : "stable";
+my $path = "$cache_dir/HC_RELEASE-$type.txt";
+
+if (-f $path) {
+    open(my $fh, '<', $path) or die "Cannot open $path: $!";
+    local $/; # Slurp the whole file
+    my $content = <$fh>;
+    close($fh);
+    print $content;
+} else {
+    print "Unknown\n";
+}
