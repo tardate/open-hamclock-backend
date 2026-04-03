12806f524d36dfc8a2570d4336e412e56dd5e092
Dave Koberstein <davek@komacke.com>
2026-04-03 15:28:50 -0700

    NASA sdo movies are back.
    Currently the HC client still goes directly to the NASA endpoint.
    However just to be prepared to support HC coming to the backend, this
    first 307's to NASA if there's a 200. Otherwise fallback to suntoday. Or
    finally 404.

--- ham/HamClock/SDO/movies/sdo-movies.pl
+++ ham/HamClock/SDO/movies/sdo-movies.pl
@@ -1,28 +1,56 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use LWP::UserAgent;
 
-# Define the mapping of filenames to their target URLs
-my %movie_map = (
-    "1024_211193171.mp4" => "https://suntoday.lmsal.com/suntoday/",
-    "1024_HMIB.mp4" => "https://suntoday.lmsal.com/suntoday/",
-    "1024_HMIIC.mp4" => "https://suntoday.lmsal.com/suntoday/",
-    "1024_0131.mp4" => "https://suntoday.lmsal.com/suntoday/",
-    "1024_0193.mp4" => "https://suntoday.lmsal.com/suntoday/",
-    "1024_0211.mp4" => "https://suntoday.lmsal.com/suntoday/",
-    "1024_0304.mp4" => "https://suntoday.lmsal.com/suntoday/",
+# 1. Primary NASA URLs (Direct links to files)
+my %primary_map = (
+    "1024_211193171.mp4" => "https://sdo.gsfc.nasa.gov/assets/img/latest/mpeg/latest_1024_211193171.mp4",
+    "1024_HMIB.mp4"      => "https://sdo.gsfc.nasa.gov/assets/img/latest/mpeg/latest_1024_HMIB.mp4",
+    "1024_HMIIC.mp4"     => "https://sdo.gsfc.nasa.gov/assets/img/latest/mpeg/latest_1024_HMIIC.mp4",
+    "1024_0131.mp4"      => "https://sdo.gsfc.nasa.gov/assets/img/latest/mpeg/latest_1024_0131.mp4",
+    "1024_0193.mp4"      => "https://sdo.gsfc.nasa.gov/assets/img/latest/mpeg/latest_1024_0193.mp4",
+    "1024_0211.mp4"      => "https://sdo.gsfc.nasa.gov/assets/img/latest/mpeg/latest_1024_0211.mp4",
+    "1024_0304.mp4"      => "https://sdo.gsfc.nasa.gov/assets/img/latest/mpeg/latest_1024_0304.mp4",
 );
 
-# Get the filename from the query string (e.g., aia171.mp4)
-my $filename = $ENV{'QUERY_STRING'};
+# 2. Fallback Base URLs (Filename will be appended)
+my %fallback_base_map = (
+    "1024_211193171.mp4" => "https://suntoday.lmsal.com/suntoday/",
+    "1024_HMIB.mp4"      => "https://suntoday.lmsal.com/suntoday/",
+    "1024_HMIIC.mp4"     => "https://suntoday.lmsal.com/suntoday/",
+    "1024_0131.mp4"      => "https://suntoday.lmsal.com/suntoday/",
+    "1024_0193.mp4"      => "https://suntoday.lmsal.com/suntoday/",
+    "1024_0211.mp4"      => "https://suntoday.lmsal.com/suntoday/",
+    "1024_0304.mp4"      => "https://suntoday.lmsal.com/suntoday/",
+);
 
-if (exists $movie_map{$filename}) {
-    # Print the 307 Redirect header
-    print "Status: 307 Temporary Redirect\n";
-    print "Location: $movie_map{$filename}\n\n";
+my $filename = $ENV{'QUERY_STRING'} || "";
+
+if (exists $primary_map{$filename}) {
+    my $ua = LWP::UserAgent->new;
+    $ua->timeout(5); 
+    
+    # Check if NASA file is alive
+    my $response = $ua->head($primary_map{$filename});
+
+    if ($response->is_success) {
+        # Success: Redirect to NASA
+        redirect_to($primary_map{$filename});
+    } else {
+        # Failure: Construct fallback URL (Base + Filename)
+        my $fallback_url = $fallback_base_map{$filename} . $filename;
+        redirect_to($fallback_url);
+    }
 } else {
-    # Fallback if the filename isn't in your list
     print "Status: 404 Not Found\n";
     print "Content-type: text/plain\n\n";
     print "Movie not found.";
+}
+
+sub redirect_to {
+    my ($url) = @_;
+    print "Status: 307 Temporary Redirect\n";
+    print "Location: $url\n\n";
+    exit;
 }
